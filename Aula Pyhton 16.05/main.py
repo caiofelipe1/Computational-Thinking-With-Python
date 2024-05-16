65e2979289a8c05658073ec2d506b36de2584bc1
Caio Felipe <160545339+caiofelipe1@users.noreply.github.com>
2024-05-16 11:53:00 -0300

    Delete Aula Pyhton 16.05/main.py

--- Aula Pyhton 16.05/main.py
+++
@@ -1,8 +0,0 @@
-def checa_numero(msg):
-    num = input()
-    while not num.isnumeric():
-        num = input(msg)
-    num = int(num)
-
-checa_numero('Diga seu ano de nascimento: ')
-checa_numero('Diga a quantidade de garrafas: ')
